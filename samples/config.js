6b1b015ec3133d07558510d978bce290eeb2fd0b
Marek Lewandowski <marek@mlewandowski.com>
2017-03-03 09:24:12 +0100

    Added a sample page.

---
+++ samples/config.js
@@ -0,0 +1,74 @@
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	// %REMOVE_START%
+	config.plugins =
+		'about,' +
+		'a11yhelp,' +
+		'basicstyles,' +
+		'bidi,' +
+		'blockquote,' +
+		'clipboard,' +
+		'colorbutton,' +
+		'colordialog,' +
+		'copyformatting,' +
+		'contextmenu,' +
+		'dialogadvtab,' +
+		'div,' +
+		'elementspath,' +
+		'enterkey,' +
+		'entities,' +
+		'filebrowser,' +
+		'find,' +
+		'flash,' +
+		'floatingspace,' +
+		'font,' +
+		'format,' +
+		'forms,' +
+		'horizontalrule,' +
+		'htmlwriter,' +
+		'image,' +
+		'iframe,' +
+		'indentlist,' +
+		'indentblock,' +
+		'justify,' +
+		'language,' +
+		'link,' +
+		'list,' +
+		'liststyle,' +
+		'magicline,' +
+		'maximize,' +
+		'newpage,' +
+		'pagebreak,' +
+		'pastefromword,' +
+		'pastetext,' +
+		'preview,' +
+		'print,' +
+		'removeformat,' +
+		'resize,' +
+		'save,' +
+		'selectall,' +
+		'showblocks,' +
+		'showborders,' +
+		'smiley,' +
+		'sourcearea,' +
+		'specialchar,' +
+		'stylescombo,' +
+		'tab,' +
+		'table,' +
+		'tabletools,' +
+		'templates,' +
+		'toolbar,' +
+		'undo,' +
+		'wysiwygarea';
+	// %REMOVE_END%
+
+	// To add any external plugin just use:
+	// CKEDITOR.plugins.addExternal( 'customplugin', '../plugins/customplugin/plugin.js' );
+	// config.plugins += ',customplugin';
+
+
+	CKEDITOR.plugins.addExternal( 'boilerplate', location.href.replace( '/index.html', '' ) + '/../plugin.js' );
+	config.plugins += ',boilerplate';
+};
